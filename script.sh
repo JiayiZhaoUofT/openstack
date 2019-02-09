31b6ac3acc4f75b08be636e42c8df15b11dd42c4
JiayiZhaoUofT <37254657+JiayiZhaoUofT@users.noreply.github.com>
2019-02-08 23:22:43 -0500

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,19 +1,19 @@
 curl -i \
--H "Content-Type: application/json" \
--d '
-{ "auth": {
-"identity": {
-"methods": ["password"],
-"password": {
-"user": {
-"name": "admin",
-"domain": { "id": "default" },
-"password": "secret"
+     -H "Content-Type: application/json" \
+     -d '
+   { "auth": {
+       "identity": {
+         "methods": ["password"],
+         "password": {
+           "user": {
+             "name": "admin",
+             "domain": { "id": "default" },
+             "password": "secret"
 } }
 }, "scope": {
-"project": {
-"name": "admin",
-"domain": { "id": "default" }
+         "project": {
+           "name": "admin",
+           "domain": { "id": "default" }
 } }
 } }' \
 http://localhost/identity/v3/auth/tokens
