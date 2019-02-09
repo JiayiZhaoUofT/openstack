340f8668a045b131ec21d9f5ac667c35090950aa
JiayiZhaoUofT <37254657+JiayiZhaoUofT@users.noreply.github.com>
2019-02-08 23:17:12 -0500

    openstack

---
+++ script.sh
@@ -0,0 +1,19 @@
+content = "$(curl -i \
+-H "Content-Type: application/json" \
+-d '
+{ "auth": {
+"identity": {
+"methods": ["password"],
+"password": {
+"user": {
+"name": "admin",
+"domain": { "id": "default" },
+"password": "secret"
+} }
+}, "scope": {
+"project": {
+"name": "admin",
+"domain": { "id": "default" }
+} }
+} }' \
+http://localhost/identity/v3/auth/tokens)"
