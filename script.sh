0a0857d9b2cc78ad79d79151650327a9736041c2
JiayiZhaoUofT <37254657+JiayiZhaoUofT@users.noreply.github.com>
2019-02-10 14:25:47 -0500

    Update script.sh

--- script.sh
+++ script.sh
@@ -17,19 +17,18 @@ content=$(curl -i \
            "domain": { "id": "default" }
 } }
 } }' \
-http://localhost/identity/v3/auth/tokens | grep "X-Subject-Token:" | awk -F":" '{ print $2 }')
-# | grep -oP '(X-Subject-Token: \K\w+).*')
-export OS_TOKEN=${content//$'\015'}
+http://localhost/identity/v3/auth/tokens | grep -oP '(X-Subject-Token: \K\w+).*')
+export OS_TOKEN=$content
 
-curl -s \
--H "Content-Type: application/json" \
--H "X-Auth-Token:$OS_TOKEN" \
--d '
-{
-"subnet": {
-"network_id": "d32019d3-bc6e-4319-9c1d-6722fc136a22",
+# curl -s \
+# -H "Content-Type: application/json" \
+# -H "X-Auth-Token:$OS_TOKEN" \
+# -d '
+# {
+# "subnet": {
+# "network_id": "d32019d3-bc6e-4319-9c1d-6722fc136a22",
 
-"ip_version": 4,
-"cidr": "192.168.199.0/24"
-}
-}' "http://localhost:9696/v2.0/subnets" ; echo
+# "ip_version": 4,
+# "cidr": "192.168.199.0/24"
+# }
+# }' "http://localhost:9696/v2.0/subnets" ; echo
