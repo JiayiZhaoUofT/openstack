e33f203bbca7c35d729520f898500b09773bc6ea
JiayiZhaoUofT <37254657+JiayiZhaoUofT@users.noreply.github.com>
2019-02-10 14:40:18 -0500

    Create script1.sh

---
+++ script1.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+content=$(curl -i \
+     -H "Content-Type: application/json" \
+     -d '
+   { "auth": {
+       "identity": {
+         "methods": ["password"],
+         "password": {
+           "user": {
+             "name": "admin",
+             "domain": { "id": "default" },
+             "password": "secret"
+} }
+}, "scope": {
+         "project": {
+           "name": "admin",
+           "domain": { "id": "default" }
+} }
+} }' \
+http://localhost/identity/v3/auth/tokens | grep -oP '(X-Subject-Token: \K\w+).*')
+export OS_TOKEN=$content
+#List of VMs:
+curl -s -H "X-Auth-Token: $OS_TOKEN" "http://localhost/compute/v2.1/networks" | python -mjson.tool
