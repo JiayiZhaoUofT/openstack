945d4aaa1e7da06c2c9a43268d5f71fa2e31e6fc
JiayiZhaoUofT <37254657+JiayiZhaoUofT@users.noreply.github.com>
2019-02-10 14:30:49 -0500

    Update script.sh

--- script.sh
+++ script.sh
@@ -20,15 +20,15 @@ content=$(curl -i \
 http://localhost/identity/v3/auth/tokens | grep -oP '(X-Subject-Token: \K\w+).*')
 export OS_TOKEN=$content
 echo $OS_TOKEN
-# curl -s \
-# -H "Content-Type: application/json" \
-# -H "X-Auth-Token:$OS_TOKEN" \
-# -d '
-# {
-# "subnet": {
-# "network_id": "d32019d3-bc6e-4319-9c1d-6722fc136a22",
+curl -s \
+-H "Content-Type: application/json" \
+-H "X-Auth-Token:$OS_TOKEN" \
+-d '
+{
+"subnet": {
+"network_id": "d32019d3-bc6e-4319-9c1d-6722fc136a22",
 
-# "ip_version": 4,
-# "cidr": "192.168.199.0/24"
-# }
-# }' "http://localhost:9696/v2.0/subnets" ; echo
+"ip_version": 4,
+"cidr": "192.168.199.0/24"
+}
+}' "http://localhost:9696/v2.0/subnets" ; echo
